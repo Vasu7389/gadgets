5eadad8c0547117e8ac2f38104dae1fed3378501
Vasu7389 <awasthivasu.65@gmail.com>
2021-06-27 21:56:30 +0530

    Warning resolved

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card } from "react-bootstrap";
 import RatingStars from "./common/RatingStars";
 import "./ProductCard.css";
 
